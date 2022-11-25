4e9aab809b5b3cde69808b4d16c404a319eeeada
Rafael Queiroz <wrafael.queiroz@icloud.com>
2022-11-25 23:27:21 +0400

    Add files via upload

---
+++ day12_mystery_number.py
@@ -0,0 +1,41 @@
+import art
+import random
+
+def compare(num, guess):
+	if num == guess:
+		print("Spot on. Your guess was correct.")
+	elif num > guess:
+		print("Too low.")
+	elif num < guess:
+		print("Too high.")
+
+print(art.logo)
+
+print("\nI am thinking of a number from 1 to 100. Dare to guess?")
+num = random.randint(1, 101)
+print(num)
+while True:
+	level = input("Choose a level 'easy' or 'hard': ").lower()
+	if level == 'easy':
+		attempts = 10
+		break
+	elif level == 'hard':
+		attempts = 5
+		break
+	else:
+		print("\nIncorrect entry. Try again")
+print(f"\nYou have {attempts} chances to get this right.")
+
+game_over = False
+
+
+while attempts > 0 and game_over == False:
+	guess = int(input("Make a guess: "))
+	compare(num, guess)
+	attempts -= 1
+	if num == guess:
+		game_over = True
+
+if attempts == 0:
+	print("Too bad, you lost.")
+print(f"My number was: {num}")
